20271c4b074422d153bca35805e63594d320f761
Drew Williams <awilliams1789@gmail.com>
2022-02-08 23:38:01 -0600

    P0 - Final

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+
+
+}
+
--- src/client/modules/my/mainPage/mainPage.js
+++ src/client/modules/my/mainPage/mainPage.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class mainPage extends LightningElement {}
+
+export default class mainPage extends LightningElement {
+    buttonClicked = false;
+
+    handleClicked(e) {
+        if(this.buttonClicked == true) {
+            this.buttonClicked = false;
+        } else {
+        this.buttonClicked = true;
+        }
+
+        if(e.target.style.color == "red"){
+        //method to add to cart
+            e.target.style.color = "black";
+            e.target.innerHTML = "Add to Cart";
+
+        } else {
+        //method to remove from cart
+            e.target.style.color = "red";
+            e.target.innerHTML = "Remove from Cart";
+        }
+    }
+    
+}
+
